7b263aae6445e8fb5d55232e18815fecdd8c19fb
Keenan Irvin-Benning <kirvinb@outlook.com>
2018-07-24 19:44:17 -0400

    Adding styles and other clean up

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList  } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
-        <div className="demo-big-content">
-          <Layout>
-            <Header className="header-color" title="My Portfolio" scroll Link="/">
-              <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/contact">Contact</Link>
-              </Navigation>
-            </Header>
-            <Drawer title="Title">
-              <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/contact">Contact</Link>
-              </Navigation>
-            </Drawer>
-            <Content>
-              <div className="page-content" />
-              <Main></Main>
+    render() {
+        return (
+            <div>
+                {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
+                <div className="demo-big-content">
+                    <Layout>
+                        <Header className="header-color" title="My Portfolio" scroll Link="/">
+                            <Navigation>
+                                <Link to="/resume">Resume</Link>
+                                <Link to="/projects">Projects</Link>
+                                <Link to="/aboutme">About Me</Link>
+                                <Link to="/contact">Contact</Link>
+                            </Navigation>
+                        </Header>
+                        <Drawer title="Title">
+                            <Navigation>
+                                <Link to="/resume">Resume</Link>
+                                <Link to="/projects">Projects</Link>
+                                <Link to="/aboutme">About Me</Link>
+                                <Link to="/contact">Contact</Link>
+                            </Navigation>
+                        </Drawer>
+                        <Content>
+                            <div className="page-content" />
+                            <Main></Main>
 
-            </Content>
-            <Footer size="mini">
-              <FooterSection type="left" logo="Title">
-                <FooterLinkList>
-                  <a href="#">Help</a>
-                  <a href="#">Privacy & Terms</a>
-                </FooterLinkList>
-              </FooterSection>
-            </Footer>
+                        </Content>
+                        <Footer size="mini">
+                            <FooterSection type="left" logo="Title">
+                                <FooterLinkList>
+                                    <Link to="/">Home</Link>
+                                    <Link to="/Contact">Privacy & Terms</Link>
+                                </FooterLinkList>
+                            </FooterSection>
+                        </Footer>
 
-          </Layout>
-        </div>
+                    </Layout>
+                </div>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,11 +6,11 @@ class Landing extends Component {
         return (
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
+                    <Cell col={12}>
                         <img src="https://image.flaticon.com/icons/svg/53/53060.svg"
-                        alt="avatar"
-                        className="avatar-img" />
-
+                            alt="avatar"
+                            className="avatar-img" />
+                    </Cell>
                         <div className="banner-text">
                         <h1>Full Stack Web Developer</h1>
                         <hr/>
@@ -18,6 +18,23 @@ class Landing extends Component {
                         <p>
                             .ASP.NET | C#/VB.NET | Javascript | SQL Server | Microsoft Azure | React | NodeJS
                         </p>
+                        <div className="social-links">
+                            {/*LinkedIn  */}
+                            <a href="https://www.linkedin.com/in/keenanirvin" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
+
+                            {/*Github  */}
+                            <a href="https://github.com/icianan" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
+                            {/*Gmail  */}
+                            <a href="https://gmail.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-envelope" aria-hidden="true" />
+                            </a>
+
+
+                        </div>
 
                         </div>
                 </Grid>
