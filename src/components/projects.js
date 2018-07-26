3d8666d30842d972fe42834e2863e6ed0e6b5d0f
Keenan Irvin-Benning <kirvinb@outlook.com>
2018-07-26 00:25:51 -0400

    added tabs to project component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,25 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
-    class Project extends Component{
-        render() {
-            return(
-                <div><h1>Project Page </h1></div>
-            )
-        }
-
+class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 }
     }
-    export default Project;
+    render() {
+        return (
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={( tabId ) => this.setState({ activeTab: tabId })} ripple >
+                    <Tab>ASP.NET</Tab>
+                    <Tab>React</Tab>
+                    <Tab>Solidity</Tab>
+                    <Tab>Python</Tab>
+
+                </Tabs>
+            </div >
+
+                )
+    }
+
+}
+export default Project;
